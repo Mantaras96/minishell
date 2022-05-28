1e2ed6bf4507e4645e0bc3c7077164d4c5e5d6fa
Albert <albert.mantaras@gmail.com>
2022-05-28 13:27:18 +0200

    Echo medio creado

--- src/get_echo.c
+++ src/get_echo.c
@@ -2,19 +2,29 @@
 
 void get_echo(char *str)
 {
-    char **splitted;
     int i;
-    //int len;
+    int len;
+    int flag;
 
-    splitted = ft_split(str, ' ');
-    //len = ft_strlen(splitted);
-    //printf("%d",len);
-    if (!ft_strcmp(splitted[0], "echo")){
-        i = 1;
-        while(splitted[i]){
-            printf("%s", splitted[i]);
-            i++;
+    flag = 0;
+    len = 0;
+    i = 0;
+
+
+    if (!ft_strcmp(ft_split(str, ' ')[1], "-n"))
+        flag = 1;
+    while (str[i])
+    {
+        if( ft_isalpha(str[i]) || str[i] == '-')
+            len++;
+        if (((len > 4 && flag == 0) || (len > 6 && flag == 1)) && (i != (int)ft_strlen(str) - 1)) {
+            write(1, &str[i], 1);
         }
-        printf("\n");
+        i++;
     }
+    if (str[i - 1] != 34 && str[i - 1] != 39)
+        printf("%c\n", str[i - 1]);
+    else
+        printf("\n");
+
 }
