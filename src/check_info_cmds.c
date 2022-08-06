4918de40ddd0819d7c6855b128cf7f5fe9fbc9c5
Teo Merida rodriguez <tmerida-@cbr7s1.42barcelona.com>
2022-08-06 12:33:42 +0200

    pipes

--- src/check_info_cmds.c
+++ src/check_info_cmds.c
@@ -9,21 +9,21 @@
 void    checkInfoCdms(t_list *cmd)
 {
 	t_commands	*c;
-    int i
+    int i;
 
     i = 0;
 
     while (cmd)
     {
     t_commands *c;
-    c = (t_commands)cmds->content;
+    c = cmd->content;
     while (c->full_cmd[i]){
         printf ("Full_cmd[%d]: %s\n", i, c->full_cmd[i]);
         i++;
     }
-    printf("Full path: %s", c->full_cmd );
+    printf("Full path: %s", c->full_path );
 
-    printf ("IntFile: %d Outfile: %d", c->ifile, c->ofile);
+    printf ("IntFile: %d Outfile: %d\n", c->ifile, c->ofile);
     cmd = cmd->next;
     }
 }
