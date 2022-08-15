80cac362506fe791b695ca317f4e0327e69a47e3
Albert <albert.mantaras@gmail.com>
2022-08-15 13:25:52 +0200

    Fix norminette

--- include/minishell.h
+++ include/minishell.h
@@ -1,16 +1,31 @@
-#	include "../ft_printf/include/ft_printf.h"
-#	include "../ft_printf/include/libft.h"
-#	include "../42_get_next_line/get_next_line.h"
-#   include <readline/history.h>
-#   include <readline/readline.h>
-# 	include <limits.h>
-#	include <stdio.h>
-#   include <fcntl.h>
-#	include <term.h>
-#	include <termios.h>
-#	include <signal.h>
-#	include <dirent.h>
-#	include <sys/stat.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amantara <amantara@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/15 13:21:51 by amantara          #+#    #+#             */
+/*   Updated: 2022/08/15 13:24:59 by amantara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../ft_printf/include/ft_printf.h"
+# include "../ft_printf/include/libft.h"
+# include "../42_get_next_line/get_next_line.h"
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <limits.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <term.h>
+# include <termios.h>
+# include <signal.h>
+# include <dirent.h>
+# include <sys/stat.h>
 # include <sys/ioctl.h>
 
 typedef struct s_info
@@ -21,14 +36,13 @@ typedef struct s_info
 	int		doubles;
 	int		counter;
 	int		words;
-    int		pipe_command;
-    int		redirect;
+	int		pipe_command;
+	int		redirect;
 	char	**envp;
 	char	*prompt;
 	int		pid;
 	t_list	*cmds;
 }			t_info;
-
 
 typedef struct s_commands
 {
@@ -47,100 +61,75 @@ typedef struct s_split
 	int	size;
 }	t_split;
 
-
-int     get_pwd();
-int     ft_strcmp(const char *str1, const char *str2);
-void    print_star_minishell();
-int    	get_echo(t_commands *info);
-int	parsing(t_info *info);
-void	counter(t_info *info);
-int		simples(int i, t_info *info);
-int		doubles(int i, t_info *info);
-void	create_tokens(t_info *info);
-int		create_doubles(int i, t_info *info);
-int		create_simples (int i, t_info *info);
-int		create_word(t_info *info, int i, char charact);
-int		word_length(t_info *info, int i, char charact);
-void init_prompt(t_info *info);
-void handler(int signum);
-
+int			get_pwd(void);
+int			ft_strcmp(const char *str1, const char *str2);
+void		print_star_minishell(void);
+int			get_echo(t_commands *info);
+int			parsing(t_info *info);
+void		counter(t_info *info);
+int			simples(int i, t_info *info);
+int			doubles(int i, t_info *info);
+void		create_tokens(t_info *info);
+int			create_doubles(int i, t_info *info);
+int			create_simples(int i, t_info *info);
+int			create_word(t_info *info, int i, char charact);
+int			word_length(t_info *info, int i, char charact);
+void		init_prompt(t_info *info);
+void		handler(int signum);
 //Utils matrix (utils_matrix.c)
-void	free_matrix(char ***matrix);
-char	**add_value_matrix(char **matrix, char *newstr);
-int		ft_matrix_len(char **matrix);
-char	**ft_strdup_matrix(char **matrix);
-char	**matrix_delete_element(char **matrix, int j);
+void		free_matrix(char ***matrix);
+char		**add_value_matrix(char **matrix, char *newstr);
+int			ft_matrix_len(char **matrix);
+char		**ft_strdup_matrix(char **matrix);
+char		**matrix_delete_element(char **matrix, int j);
 //Get env (get_env.c)
-char	**set_env(char *var, char *value, char **envp, int n);
-int	get_env(t_info *info);
-
-int		search_value_string(char *argv, int value);
-char	*get_env_value(char *var, char **envp, int n);
-int		count_var_envp(char *token, char **envp, int *j);
-int	get_exit(t_info *info, int *num_exit);
-void	free_tokens(t_info *info);
-int count_var_envp(char *token, char **envp, int *j);
-
+char		**set_env(char *var, char *value, char **envp, int n);
+int			get_env(t_info *info);
+int			search_value_string(char *argv, int value);
+char		*get_env_value(char *var, char **envp, int n);
+int			count_var_envp(char *token, char **envp, int *j);
+int			get_exit(t_info *info, int *num_exit);
+void		free_tokens(t_info *info);
+int			count_var_envp(char *token, char **envp, int *j);
 //Get (export.c)
-int count_var_envp(char *token, char **envp, int *j);
-int	get_unset(t_info *info, char **full_cmd);
-int get_export(t_info *info);
-int search_value_string(char *argv, int value);
-
-int get_cd(t_info *info);
-int go_home();
-void update_pwd(t_info *info);
+int			count_var_envp(char *token, char **envp, int *j);
+int			get_unset(t_info *info, char **full_cmd);
+int			get_export(t_info *info);
+int			search_value_string(char *argv, int value);
+int			get_cd(t_info *info);
+int			go_home(void);
+void		update_pwd(t_info *info);
 //Expanding
-int		expanding (t_info *info);
-char	*expand_home(char *str, int i, int quotes[2], t_info *info);
-void	execute_other_commands(t_info *info);
-void	get_other(t_info *info);
-char 	*validate_command(char **paths, char *command);
-void 	get_path(t_info *info);
+int			expanding(t_info *info);
+char		*expand_home(char *str, int i, int quotes[2], t_info *info);
+void		execute_other_commands(t_info *info);
+void		get_other(t_info *info);
+char		*validate_command(char **paths, char *command);
+void		get_path(t_info *info);
+int			is_builtin(t_commands *n);
+t_list		*create_nodes(t_info *info, int i, t_list *cmd_1, t_list *cmd_2);
+t_commands	*command_init(void);
+t_commands	*get_outfile2(t_commands *node, char **temp2, int *i);
+t_commands	*get_outfile1(t_commands *node, char **temp2, int *i);
+t_commands	*get_infile2(t_commands *node, char **temp2, int *i);
+t_commands	*get_infile1(t_commands *node, char **temp2, int *i);
+int			get_fd(int oldfd, char *path, int flags[2]);
+void		*validate_fork(t_info *info, t_list *cmds, int p_fd[2]);
+int			builtin(t_info *info, t_list *cmds, int *num_exit);
+void		*check_status(t_info *info, t_list *cmds);
+char		**ft_cmdsubsplit(char const *s, char *set);
+char		**ft_matrix_replace_in(char ***big, char **small, int n);
+char		**remove_quotes(char **tokens);
+void		*start_args(char *str, t_info *info);
+void		free_cnt(void *content);
+int			heredoc(char *str[2], char *aux[2]);
+char		*here_str(char *str[2], size_t len, char *aux_1, char *aux_2);
+void		*error(int err);
+void		checkInfoCdms(t_list *cmd);
+t_split		start_split(void);
+void		init_info(t_info *info, char **envp, char **argv);
+int			signal_handler(void);
+void		*exec_command(t_info *info, t_list *cmds);
+void		init_env(t_info *info, char **argv);
 
-int is_builtin(t_commands *n);
-
-t_list	*create_nodes(t_info *info, int i, t_list *cmd_1, t_list *cmd_2);
-t_commands *command_init(void);
-
-t_commands *get_outfile2(t_commands *node, char **temp2, int *i);
-t_commands *get_outfile1(t_commands *node, char **temp2, int *i);
-t_commands *get_infile2(t_commands *node, char **temp2, int *i);
-t_commands *get_infile1(t_commands *node, char **temp2, int *i);
-
-int	get_fd(int oldfd, char *path, int flags[2]);
-
-void *validate_fork(t_info *info, t_list *cmds, int p_fd[2]);
-
-int builtin(t_info *info, t_list *cmds, int *num_exit);
-void *check_status(t_info *info, t_list *cmds);
-
-char	**ft_cmdsubsplit(char const *s, char *set);
-
-char	**ft_matrix_replace_in(char ***big, char **small, int n);
-
-char	**remove_quotes(char **tokens);
-
-void    *start_args(char *str, t_info *info);
-
-
-void free_cnt(void *content);
-
-
-int heredoc(char *str[2], char *aux[2]);
-
-char *here_str(char *str[2], size_t len, char *aux_1, char * aux_2);
-
-void	*error(int err);
-
-void    checkInfoCdms(t_list *cmd);
-
-t_split	start_split();
-
-void	init_info(t_info *info, char **envp, char **argv);
-
-int	signal_handler(void);
-
-void	*exec_command(t_info *info, t_list *cmds);
-
-void	init_env(t_info *info, char **argv);
+#endif
